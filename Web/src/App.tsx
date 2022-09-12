0875cbd6a8645cb6f2a351096e37e8cb3ec88de1
Maaacs <msb@icomp.ufam.edu.br>
2022-09-12 10:11:14 -0400

    Propriedades e componentes

--- Web/src/App.tsx
+++ Web/src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+
+interface ButtonPropriets{//propriedades
+  title: string;
+}
+
+function Button(props: ButtonPropriets){//componente
+  return (
+      <button>
+        {props.title}
+      </button>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  /*return <h1>Hello Max</h1>*/
   return (
-    <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Button title="Send 1" />
+        <Button title="Send 2"/>
+        <Button title="Send 3"/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
   )
+  /*return <Button/>*/
 }
 
 export default App

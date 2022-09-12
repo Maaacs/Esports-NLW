831b0d5a1eb09430a55c42e755f34fbdb1a32b0d
Maaacs <msb@icomp.ufam.edu.br>
2022-09-12 09:33:57 -0400

    Ambiente Node completo

--- Back-end/src/server.ts
+++ Back-end/src/server.ts
@@ -13,6 +13,9 @@ app.get('/ads', (request, response) => { //request recebe uma informação, resp
         {id: 1, nome: 'Anúncio 1'},
         {id: 2, nome: 'Anúncio 2'},
         {id: 3, nome: 'Anúncio 3'},
+        {id: 4, nome: 'Anúncio 4'},
+        {id: 5, nome: 'Anúncio 5'},
+
     ])
 } )
 //em desenvolvimento cada aplicação irá roda em uma porta 'localhost:3333' ex

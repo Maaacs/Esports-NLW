3ce87fda13c915520658886cf84f0f514e41f620
Maaacs <msb@icomp.ufam.edu.br>
2022-09-13 14:54:56 -0400

    Limpando app

--- Web/src/App.tsx
+++ Web/src/App.tsx
@@ -1,26 +1,12 @@
 import './styles.css'
-interface ButtonPropriets{//propriedades
-  title: string;
-}
 
-function Button(props: ButtonPropriets){//componente
-  return (
-      <button>
-        {props.title}
-      </button>
-  )
-}
 
 function App() {
-  /*return <h1>Hello Max</h1>*/
   return (
       <div>
-        <Button title="Send 1" />
-        <Button title="Send 2"/>
-        <Button title="Send 3"/>
+        
       </div>
   )
-  /*return <Button/>*/
 }
 
 export default App

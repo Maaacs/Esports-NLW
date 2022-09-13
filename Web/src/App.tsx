73f01a0d793cb82de495edbb54fde5a8afdcb9c4
Maaacs <msb@icomp.ufam.edu.br>
2022-09-13 17:16:41 -0400

    bordas dos jogos

--- Web/src/App.tsx
+++ Web/src/App.tsx
@@ -10,24 +10,30 @@ function App() {
       <h1 className="text-6xl text-white font-black mt-20">
         Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui.
         </h1>
-
         <div className="grid grid-cols-6 gap-6">
-          <a href="">
+          <a href="" className="relative">
             <img src="/game-1.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradiente absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">League of Legends</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>  
+
+
           </a>
-          <a href="">
+          <a href="" className="relative">
             <img src="/game-2.png" alt="" />
           </a>
-          <a href="">
+          <a href="" className="relative">
             <img src="/game-3.png" alt="" />
           </a>
-          <a href="">
+          <a href="" className="relative">
             <img src="/game-4.png" alt="" />
           </a>
-          <a href= "">
+          <a href="" className="relative">
             <img src="/game-5.png" alt="" />
           </a>
-          <a href= "">
+          <a href="" className="relative">
             <img src="/game-6.png" alt="" />
           </a>
         </div>

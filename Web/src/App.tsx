8c520cb97aef902479c82f2982ef66ab72a81266
Maaacs <msb@icomp.ufam.edu.br>
2022-09-15 13:34:50 -0400

    Texto e botao da box nao encontrou seu duo

--- Web/src/App.tsx
+++ Web/src/App.tsx
@@ -66,9 +66,16 @@ function App() {
       </div>
 
       <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg overflow-hidden mt-8 ">
-      <div className= "bg-[#2A2634] px-8 py-6"> </div>
+        <div className= "bg-[#2A2634] px-8 py-6 flex justify-between items-center"> 
+          <div>
+            <strong className="text-2xl text-white font-black block">Não encontrou seu duo?</strong>
+            <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
+          </div>
+          <button className="py-3 px-4 bg-violet-500 text-white rounded">
+            Publicar anúncio
+          </button>
+        </div>
       </div>
-      
     </div>
   )
 }

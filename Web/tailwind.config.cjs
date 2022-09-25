fe46ae7616cceaecbbff9cd9b619262ed2905ca7
Maaacs <msb@icomp.ufam.edu.br>
2022-09-25 12:54:38 -0400

    Lupa botao publicar

--- Web/tailwind.config.cjs
+++ Web/tailwind.config.cjs
@@ -11,7 +11,7 @@ module.exports = {
         galaxy: "url('/background-galaxy.png')",
         'nlw-gradient': 'linear-gradient(90deg, #9572FC 0%, #43E7AD 50.52%, #E2D45C 100%)',
         'game-gradiente' : 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
-      },
+      }, 
     },
   },
   plugins: [],

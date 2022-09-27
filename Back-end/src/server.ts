367fde3800afae729ca2fee0a14c20ba10972391
Maaacs <msb@icomp.ufam.edu.br>
2022-09-27 14:11:30 -0400

    Finalizando Back-End

--- Back-end/src/server.ts
+++ Back-end/src/server.ts
@@ -3,34 +3,105 @@
 //Para importação utilizando module é necessário que o nome do arquivo seja .mjs
 import express, { request, response } from 'express'
 
+import {PrismaClient} from '@prisma/client' // Para acessar o banco
+import { convertHourStringToMinutes } from './utils/convert-hour-string-to-minutes'
+import { convertMinutesToHourString } from './utils/convert-minutes-to-hour-string'
+
+
+
 const app = express()
 
+app.use(express.json())
 
-app.get('/games', (request, response) => {
-    return response.json(['a']);
+const prisma = new PrismaClient() // faz a conexão com o banco
+
+
+app.get('/games', async (request, response) => {
+    const games = await prisma.game.findMany({
+        include: {
+            _count:{
+                select:{
+                    ads: true,
+                }
+            }
+        }
+    }) //passar instruções do Mysql
+
+    return response.json(games);
+});
+
+
+app.post('/games/:id/ads', async (request, response) => {
+
+    const gameId = request.params.id;
+    const body: any = request.body;
+
+    const ad = await prisma.ad.create({
+        data: {
+            gameId,
+            name: body.name,
+            yearsPlaying: body.yearsPlaying,
+            discord: body.discord,
+            weekDays: body.weekDays.join(','),
+            hourStart:  convertHourStringToMinutes(body.hourStart),
+            hourEnd: convertHourStringToMinutes(body.hourEnd),
+            useVoiceChannel: body.useVoiceChannel,
+        }
+    })
+
+    return response.status(201).json(ad);
 })
 
 
-app.post('/ads', (request, response) => {
-    return response.status(201).json(['b']);
-})
+app.get('/games/:id/ads', async (request, response) => { //request recebe uma informação, responde devolve
 
+    const gameId = request.params.id
 
-app.get('/games/:id/ads', (request, response) => { //request recebe uma informação, responde devolve
+    const ads = await prisma.ad.findMany({
+        select:{
+            id: true,
+            name: true,
+            weekDays: true,
+            useVoiceChannel: true,
+            yearsPlaying: true,
+            hourStart: true,
+            hourEnd: true,
+        },
+        where: {
+            gameId,
+        },
+        orderBy: {
+            createdAt: 'desc',
+        }
+    })
     
-    return response.json([ //Aqui podemos retornar qualquer tipo primitivo para o java (ex: array,obj,string)
-        {id: 1, nome: 'Anúncio 1'},
-        {id: 2, nome: 'Anúncio 2'},
-        {id: 3, nome: 'Anúncio 3'},
-        {id: 4, nome: 'Anúncio 4'},
-        {id: 5, nome: 'Anúncio 5'},
-
-    ])
+    return response.json(ads.map (ad => {
+        return {
+            ...ad,
+            weekDays: ad.weekDays.split(','),
+            hoursStart: convertMinutesToHourString(ad.hourStart),
+            hoursEnd: convertMinutesToHourString(ad.hourEnd),
+        }
+    }))
 })
 
 
-app.get('/ads/:id/discord', (request, response) => {
-    return response.json([])
+app.get('/ads/:id/discord', async (request, response) => {
+
+    const adId = request.params.id;
+    
+    const ad = await prisma.ad.findUniqueOrThrow({
+        select: {
+            discord: true,
+        },
+        where: {
+            id: adId,
+        }
+    })
+
+    return response.json({
+        discord: ad.discord,
+    })
 })
 
 

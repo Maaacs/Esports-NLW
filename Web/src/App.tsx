8b34b7a35343eedf48446e3015fa5a0b45d2b35c
Maaacs <msb@icomp.ufam.edu.br>
2022-09-27 15:54:56 -0400

    criando componente para os jogos

--- Web/src/App.tsx
+++ Web/src/App.tsx
@@ -3,6 +3,7 @@ import { MagnifyingGlassPlus } from 'phosphor-react'
 import './styles/main.css';
 
 import logoImg from './assets/Logo.svg'
+import { GameBanner } from './components/GameBanner';
 
 function App() {
   return (
@@ -15,56 +16,12 @@ function App() {
 
         {/* div dos jogos */}
       <div className="grid grid-cols-6 gap-6 mt-16"> 
-          <a href="" className="relative rounded-lg overflow-hidden">
-            <img src="/game-1.png" alt="" />
-
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradiente absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">Dota 2</strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div>  
-
-
-          </a>
-          <a href="" className="relative">
-            <img src="/game-2.png" alt="" />
-
-             <div className="w-full pt-16 pb-4 px-4 bg-game-gradiente absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">Counter Strike</strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div> 
-          </a>
-          <a href="" className="relative">
-            <img src="/game-3.png" alt="" />
-
-             <div className="w-full pt-16 pb-4 px-4 bg-game-gradiente absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">World of Warcraft</strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div> 
-          </a>
-          <a href="" className="relative">
-            <img src="/game-4.png" alt="" />
-
-             <div className="w-full pt-16 pb-4 px-4 bg-game-gradiente absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">League of Legends</strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div> 
-          </a>
-          <a href="" className="relative">
-            <img src="/game-5.png" alt="" />
-
-             <div className="w-full pt-16 pb-4 px-4 bg-game-gradiente absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">Apex Legends</strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div> 
-          </a>
-          <a href="" className="relative">
-            <img src="/game-6.png" alt="" />
-
-             <div className="w-full pt-16 pb-4 px-4 bg-game-gradiente absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">Fortnite</strong>
-            <span className="text-zinc-300 text-sm block">4 anúncios</span>
-          </div> 
-          </a>
+          <GameBanner bannerurl="/game-1.png" title="League of Legends" adsCount={5}/>
+          <GameBanner bannerurl="/game-2.png" title="Dota 2" adsCount={2}/>
+          <GameBanner bannerurl="/game-3.png" title="CS:GO" adsCount={1}/>
+          <GameBanner bannerurl="/game-4.png" title="World of Warcraft" adsCount={5}/>
+          <GameBanner bannerurl="/game-5.png" title="Apex" adsCount={3}/>
+          <GameBanner bannerurl="/game-6.png" title="Fortnite" adsCount={7}/>
       </div>
 
       <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg overflow-hidden mt-8 ">
